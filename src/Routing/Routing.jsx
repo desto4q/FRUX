ddf59441b93cfa3dd026ae42845257949192f3c3
desto4q <desto4q@gmail.com>
2023-03-25 20:14:46 +0100

    responsive

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from '../components/Header'
+import Mobilenav from '../components/Mobilenav'
 import Sidebar from '../components/Sidebar'
 import Dashboard from '../pages/Dashboard'
 
@@ -11,6 +12,7 @@ function Routing() {
         <Sidebar/>
         <div className="main_page">
           <Header/>
+          <Mobilenav/>
           <Routes>
               <Route path='' element={<Dashboard/>}/>    
           </Routes>
---
+++ src/components/Mobilenav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserInfo from './UserInfo'
+
+function Mobilenav() {
+  return (
+    <div className='mobile_nav'>
+        <span>
+            <UserInfo/>
+        </span>
+    </div>
+  )
+}
+
+export default Mobilenav
