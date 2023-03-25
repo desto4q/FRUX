0aca7f6d2f382c8dc58aeded2cef272e4328be4e
desto4q <desto4q@gmail.com>
2023-03-25 21:07:58 +0100

    navigation done

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from '../components/Header'
 import Mobilenav from '../components/Mobilenav'
+import M_sidebar from '../components/M_components/M_sidebar'
 import Sidebar from '../components/Sidebar'
 import Dashboard from '../pages/Dashboard'
 
@@ -13,6 +14,7 @@ function Routing() {
         <div className="main_page">
           <Header/>
           <Mobilenav/>
+          <M_sidebar/>
           <Routes>
               <Route path='' element={<Dashboard/>}/>    
           </Routes>
--- src/components/M_components/M_sidebar.jsx
+++ src/components/M_components/M_sidebar.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { Transform } from 'tabler-icons-react'
+import { AppContext } from '../../App'
+import { navLink } from '../../data/Data'
 
 function M_sidebar() {
+  let {hamState,setHam} = useContext(AppContext)
+
+  if (hamState == true) {
+    return (
+      <div className='m_sidebar' style={{transform: "translateX(0)"}}>
+        <div className="container">
+          {navLink.map(({Icon,name,to},key)=>{
+            return (
+              <div onClick={()=>{
+                setHam(!hamState)
+              }}>
+                <Link key={key} to={to} >
+                  <Icon/> <p>{name}</p>
+                </Link>
+              </div>
+            )
+  
+          })}
+        </div>
+      </div>
+    )
+  } 
   return (
-    <div>M_sidebar</div>
+    <div className='m_sidebar' style={{transform: "translateX(-120%)"}}>
+      <div className="container">
+        {navLink.map(({Icon,name,to},key)=>{
+          return (
+            <Link key={key} to={to}>
+              <Icon/> <p>{name}</p>
+            </Link>
+          )
+
+        })}
+      </div>
+    </div>
   )
 }
 
