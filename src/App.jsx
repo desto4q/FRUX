ee3e57d78ed917d429912f0e616884a51e4d181d
desto4q <desto4q@gmail.com>
2023-03-25 16:46:58 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useLayoutEffect } from 'react'
 import "./App.scss"
 import Routing from './Routing/Routing'
+import WebFont from 'webfontloader';
 
 function App() {
   useLayoutEffect(()=>{
     let title = document.querySelector("title").innerHTML = "FRUX"
+    WebFont.load({
+      google: {
+        families: ['Montserrat']
+      }
+    });
   })
   return (
     <div className="app">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <span><h1>logo</h1></span>
+      <span className='links'></span>
+      <span></span>
+    </div>
   )
 }
 
